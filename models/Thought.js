5f80d2b5c785fbb7d4caec5b3969010b63bca30e
Christian Luna <christianlunaduenas@gmail.com>
2022-11-22 15:49:55 -0800

    User and thought in my models is up to date.

---
+++ models/Thought.js
@@ -0,0 +1,57 @@
+const { Schema, Types, model } = require("mongoose");
+const formatDate = require("../utils/date");
+
+// Creating my reaction schema and reaction can only be done in schema.
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactionText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => formatDate(timestamp),
+  },
+});
+
+// Creating the thought through schema.
+const thoughtSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => formatDate(timestamp),
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: { virtuals: true, getters: true },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
